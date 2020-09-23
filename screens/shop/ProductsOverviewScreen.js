fd9b3baa067a3be2d4922048b1525ab4be1cdd6e
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-09-23 15:00:11 +0300

    Create ProductItem component.

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -2,6 +2,8 @@ import React from "react";
 import { FlatList, StyleSheet, Text } from "react-native";
 import { useSelector } from "react-redux";
 
+import ProductItem from "../../components/shop/ProductItem";
+
 const ProductsOverviewScreen = () => {
   const products = useSelector((state) => state.products.availableProducts);
 
@@ -9,14 +11,22 @@ const ProductsOverviewScreen = () => {
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(item) => <Text>{item.item.title}</Text>}
+      renderItem={({ item }) => (
+        <ProductItem
+          image={item.imageUrl}
+          title={item.title}
+          price={item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: "All products"
-}
+  headerTitle: "All products",
+};
 
 export default ProductsOverviewScreen;
 
