97a61a44031b86081f88f7c37efd611da74d6e54
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-09-23 13:55:01 +0300

    Add Product model and mock data. Create product store.

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+const ProductsOverviewScreen = () => {
+  return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
+
+const styles = StyleSheet.create({});
---
+++ store/reducers/products.js
@@ -0,0 +1,13 @@
+import PRODUCTS from "../../data/dummy-data";
+
+const initialState = {
+  availableProducts: PRODUCTS,
+  userProducts: PRODUCTS.filter((prod) => prod.ownerId === "u1"),
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
