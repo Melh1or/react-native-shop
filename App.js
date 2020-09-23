0c8bcb21a786c6bb8f3e5f1c5e6f61ba58c81c14
Trachuk Serhiy <trachukserhiy@gmail.com>
2020-09-23 16:48:36 +0300

    Configure fonts.

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
 
 import ShopNavigator from "./navigation/ShopNavigator";
 
@@ -12,7 +14,25 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    regular: require("./assets/fonts/OpenSans-Regular.ttf"),
+    bold: require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={console.error}
+      />
+    );
+  }
   return (
     <Provider store={store}>
       <ShopNavigator />
